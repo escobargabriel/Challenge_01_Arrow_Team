69b1313ba2145f08366f974e41f77a46a6d18f61
Gabriel Escobar <gabrielgbg@gmail.com>
2022-02-04 19:33:24 -0300

    Create unitary tests

---
+++ src/test/java/DataManipulationTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataManipulationTest {
+
+  @org.junit.jupiter.api.Test
+  void readCSVFile() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void vectorToAVectorSchemaRoot() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void vectorSchemaRootToACsvFileTransform() {
+  }
+}
